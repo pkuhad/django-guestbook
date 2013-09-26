e67477df1ca276f467cae449c9ba77e35f8dcd80
Paras Kuhad <paras.kuhad@changer.nl>
2013-09-26 13:20:37 +0530

    Bringing guest_posts to guestform

--- guestbook/guestbook/views.py
+++ guestbook/guestbook/views.py
@@ -7,12 +7,20 @@ from django.core.urlresolvers import reverse
 from django.views.generic.edit import FormView
 from django.http import HttpResponse
 
-from .forms import GuestbookForm
+from django.views.generic.edit import FormMixin
+from django.views.generic.list import ListView
 
+from .forms import GuestbookForm
+from .models import GuestBook
 
 class GuestbookFormView(FormView):
     form_class = GuestbookForm
     template_name = 'guestbook/contact_form.html'
+    model = GuestBook
+    
+    def get_context_data(self, **kwargs):
+        kwargs['guest_posts'] = self.model.objects.all()
+        return kwargs
 
     def form_valid(self, form):
         return HttpResponse("ok fine")
